4917776acf387b615e42eb37e4b420bee24ba781
Deanna Riddle <lriddle.deanna@gmail.com>
2020-05-07 16:10:02 -0500

    added Volunteer page

---
+++ src/components/VolunteerPage/VolunteerPage.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+
+// material components
+import { TextField, Button } from "@material-ui/core";
+
+class VolunteerPage extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    email: "",
+    password: "",
+    whyVolunteer: "",
+  };
+
+  changeRegistrationField = (fieldKey) => (event) => {
+    this.setState({
+      [fieldKey]: event.target.value,
+    });
+  };
+
+  submitNewVolunteer = (event) => {
+    event.preventDefault();
+
+    this.props.dispatch({
+      type: "VOLUNTEER_REGISTER",
+      payload: this.state,
+    });
+    this.props.history.push("/events");
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Volunteer Registration</h2>
+
+        <form onSubmit={this.submitNewVolunteer}>
+          <TextField
+            type="text"
+            placeholder="First Name"
+            label="First Name"
+            onChange={this.changeRegistrationField("FirstName")}
+          />
+
+          <TextField
+            type="text"
+            placeholder="Last Name"
+            label="Last Name"
+            onChange={this.changeRegistrationField("LastName")}
+          />
+
+          <TextField
+            type="text"
+            placeholder="Address"
+            label="Address"
+            onChange={this.changeRegistrationField("address")}
+          />
+
+          <TextField
+            type="text"
+            placeholder="Email"
+            label="Email"
+            onChange={this.changeRegistrationField("email")}
+          />
+
+          <TextField
+            type="password"
+            placeholder="Password"
+            label="Password"
+            onChange={this.changeRegistrationField("password")}
+          />
+          <TextField
+            type="text"
+            placeholder="Why Volunteer"
+            label="Why Volunteer"
+            onChange={this.changeRegistrationField("whyVolunteer")}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(NonProfitPage);
--- src/redux/sagas/registration.saga.js
+++ src/redux/sagas/registration.saga.js
@@ -48,9 +48,36 @@ function* registerNonProfit(action) {
   }
 }
 
+function* registerVolunteer(action) {
+  try {
+    // clear any existing error on the registration page
+    yield put({ type: "CLEAR_REGISTRATION_ERROR" });
+
+    // call to nonprofit API route
+    yield axios.post("/api/user/register/volunteer", action.payload);
+
+    // automatically log a user in after registration
+    yield put({
+      type: "LOGIN",
+      payload: {
+        username: action.payload.email,
+        password: action.payload.password,
+      },
+    });
+
+    // set to 'login' mode so they see the login screen
+    // after registration or after they log out
+    yield put({ type: "SET_TO_LOGIN_MODE" });
+  } catch (error) {
+    console.log("Error with user registration:", error);
+    yield put({ type: "REGISTRATION_FAILED" });
+  }
+}
+
 function* registrationSaga() {
   yield takeLatest("REGISTER", registerUser);
   yield takeLatest("NONPROFIT_REGISTER", registerNonProfit);
+  yield takeLatest("VOLUNTEER_REGISTER", registerVolunteer);
 }
 
 export default registrationSaga;
