a7fa5b8af98479eb25a61fe8cf909a8d4b18c57c
Deanna Riddle <lriddle.deanna@gmail.com>
2020-05-12 15:33:50 -0500

    filling in content

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,50 +1,57 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
-import './LandingPage.css';
+import "./LandingPage.css";
 
 class LandingPage extends Component {
-    state = {
-        heading: 'Class Component',
-    };
+  state = {
+    heading: "About VolunTree",
+  };
 
-    onLogin = (event) => {
-        this.props.history.push('/login');
-    }
+  onLogin = (event) => {
+    this.props.history.push("/login");
+  };
 
-    render() {
-        return (
-            <div className="container">
-                <h2>{this.state.heading}</h2>
+  render() {
+    return (
+      <div className="container">
+        <h2>{this.state.heading}</h2>
 
-                <div className="grid">
-                    <div className="grid-col grid-col_8">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus ut ex molestie blandit. Etiam et turpis sit amet risus mollis interdum. Suspendisse et justo vitae metus bibendum fringilla sed sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio elementum eget. Praesent efficitur eros vitae nunc interdum, eu interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur. Quisque eget eros metus. Vestibulum bibendum fringilla nibh a luctus. Duis a sapien metus.
-                        </p>
+        <div className="grid">
+          <div className="grid-col grid-col_8">
+            <p>
+              Our founder Deanna Riddle discovered a need to connect Non-Profit
+              Organizations and Volunteers. Deanna had the desire to volunteer
+              her time, with a busy schedule it proved difficult to seek an
+              Organization to work with. As a result VolunTree was born.
+            </p>
 
-                        <p>
-                            Praesent consectetur orci dui, id elementum eros facilisis id. Sed id dolor in augue porttitor faucibus eget sit amet ante. Nunc consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut finibus metus facilisis. Nullam eget lectus non urna rhoncus accumsan quis id massa. Curabitur sit amet dolor nisl. Proin euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed lobortis augue mi vel felis. Duis ultrices sapien at est convallis congue.
-                        </p>
+            <p>
+              VolunTree is an application that connects Volunteers with
+              Non-Profit Organizations. Volunteers can register with the site
+              then look at the Events page to find an event they would like to
+              volunteer at. The volunteer will in turn contact the Organization
+              and sign up for the event they choose.
+            </p>
 
-                        <p>
-                            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat. Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed sagittis neque id diam euismod, ut egestas felis ultricies. Nullam non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit amet nisi.
-                        </p>
-                    </div>
-                    <div className="grid-col grid-col_4">
-                        <h3>Already a Member?</h3>
-                        <button
-                            className="btn btn_sizeFull"
-                            onClick={this.onLogin}
-                        >
-                            Login
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+            <p>
+              Non-Profit Organizations are able to register with the site.
+              Organizations must register with their name, main point of
+              contact, and contact information. Then they will be able to add
+              new events to the site.
+            </p>
+          </div>
+          <div className="grid-col grid-col_4">
+            <h3>Already a Member?</h3>
+            <button className="btn btn_sizeFull" onClick={this.onLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStoreToProps)(LandingPage);
