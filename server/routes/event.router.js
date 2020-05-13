14b25259f26dbe83a6a80a1108e6f0a3fc6ac1bf
Deanna Riddle <lriddle.deanna@gmail.com>
2020-05-13 17:22:03 -0500

    debuggin event router

--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -13,31 +13,31 @@ router.get("/", (req, res) => {});
 router.post("/", (req, res) => {
   console.log("Get it!", req.body);
 
-  const queryString = `INSERT INTO "events" 
-  ("event_name", "event_date", "event_type", event_address",
-  "starting_time", "ending_time",  
-  "job_one","num_job_one",
-  "job_two","num_job_two",
-  "job_three","num_job_three", 
-  "job_four","num_job_four") 
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14 )`;
+  const queryString = `INSERT INTO "events"
+  ("eventName", "event_date", "event_type", "event_Address",
+  "starting_time", "ending_time",
+  "job_one", "num_job_one",
+  "job_two", "num_job_two",
+  "job_three", "num_job_three",
+  "job_four", "num_job_four")
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);`;
 
   pool
     .query(queryString, [
-      eventData.eventName,
-      eventData.eventDate,
-      eventData.eventType,
-      eventData.eventAddress,
-      eventData.startingTime,
-      eventData.endingTime,
-      eventData.jobOne,
-      eventData.numOfJobOne,
-      eventData.jobTwo,
-      eventData.numOfJobTwo,
-      eventData.jobThree,
-      eventData.numOfJobThree,
-      eventData.jobFour,
-      eventData.numOfJobFour,
+      req.body.eventName,
+      req.body.eventDate,
+      req.body.eventType,
+      req.body.eventAddress,
+      req.body.startingTime,
+      req.body.endingTime,
+      req.body.jobOne,
+      parseInt(req.body.numOfJobOne),
+      req.body.jobTwo,
+      parseInt(req.body.numOfJobTwo),
+      req.body.jobThree,
+      parseInt(req.body.numOfJobThree),
+      req.body.jobFour,
+      parseInt(req.body.numOfJobFour),
     ])
     .then((response) => {
       res.sendStatus(201);
@@ -46,8 +46,6 @@ router.post("/", (req, res) => {
       console.log(err);
       res.sendStatus(500);
     });
-
-  res.sendStatus(201);
 });
 
 module.exports = router;
